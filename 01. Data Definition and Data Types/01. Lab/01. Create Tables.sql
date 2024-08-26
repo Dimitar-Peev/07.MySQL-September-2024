21707c2105a99d63d108d045854e19675ef63a9f
Dimitar-Peev <mctrix87@abv.bg>
2024-08-26 16:49:11 +0300

    Create "01. Create Tables.sql"

---
+++ 01. Data Definition and Data Types/01. Lab/01. Create Tables.sql
@@ -0,0 +1,25 @@
+USE `gamebar`;
+--
+CREATE TABLE `employees`
+(
+    `id`         INT AUTO_INCREMENT,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name`  VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `categories`
+(
+    `id`   INT AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `products`
+(
+    `id`          INT AUTO_INCREMENT,
+    `name`        VARCHAR(100) NOT NULL,
+    `category_id` INT          NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
