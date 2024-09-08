347394e46098d6f4f7d249e64e5458cff6d1e55b
Dimitar-Peev <mctrix87@abv.bg>
2024-09-09 01:10:18 +0300

    Update "10. Countries Holding 'A' 3 or More Times.sql"

--- 03. Built-in Functions/02. Exercise/10. Countries Holding 'A' 3 or More Times.sql
+++ 03. Built-in Functions/02. Exercise/10. Countries Holding 'A' 3 or More Times.sql
@@ -4,11 +4,3 @@ SELECT `country_name`, `iso_code`
 FROM `countries`
 WHERE `country_name` LIKE '%A%A%A%'
 ORDER BY `iso_code`;
-
--- Second way
--- SELECT `country_name`, `iso_code`
--- FROM `countries`
--- WHERE LENGTH(`country_name`) - LENGTH(REPLACE(LOWER(`country_name`), 'a', '')) >= 3
--- ORDER BY `iso_code`;
-
-
