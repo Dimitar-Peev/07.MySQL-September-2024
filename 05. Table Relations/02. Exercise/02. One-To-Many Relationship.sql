1450989b20b325e7ff9c58bc159906008a10f783
Dimitar-Peev <mctrix87@abv.bg>
2024-09-17 06:50:39 +0300

    Create "02. One-To-Many Relationship.sql"

---
+++ 05. Table Relations/02. Exercise/02. One-To-Many Relationship.sql
@@ -0,0 +1,50 @@
+USE `table_relations`;
+--
+CREATE TABLE `manufacturers`
+(
+    `manufacturer_id` INT AUTO_INCREMENT UNIQUE,
+    `name`            VARCHAR(50),
+    `established_on`  DATE
+);
+
+CREATE TABLE `models`
+(
+    `model_id`        INT AUTO_INCREMENT UNIQUE,
+    `name`            VARCHAR(50),
+    `manufacturer_id` INT
+);
+
+ALTER TABLE `models`
+    AUTO_INCREMENT = 101;
+
+INSERT INTO `manufacturers` (`name`, `established_on`)
+VALUES ('BMW', '1916-03-01'),
+       ('Tesla', '2003-01-01'),
+       ('Lada', '1966-05-01');
+
+INSERT INTO `models` (`name`, `manufacturer_id`)
+VALUES ('X1', 1),
+       ('i6', 1),
+       ('Model S', 2),
+       ('Model X', 2),
+       ('Model 3', 2),
+       ('Nova', 3);
+
+ALTER TABLE `manufacturers`
+    ADD CONSTRAINT pk_manufacturer
+        PRIMARY KEY (`manufacturer_id`);
+
+ALTER TABLE `models`
+    ADD CONSTRAINT pk_model
+        PRIMARY KEY (`model_id`);
+
+ALTER TABLE `models`
+    ADD CONSTRAINT fk_model_manufacturer
+        FOREIGN KEY (`manufacturer_id`)
+            REFERENCES `manufacturers` (`manufacturer_id`);
+--
+SELECT *
+FROM `manufacturers`;
+
+SELECT *
+FROM `models`;
