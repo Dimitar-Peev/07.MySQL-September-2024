f3dc55c08b28268583c4de47e933aceba7a1415d
Dimitar-Peev <mctrix87@abv.bg>
2024-09-23 16:58:28 +0300

    Update "02. Employees Promotion.sql"

--- 07. Database Programmability and Transactions/01. Lab/02. Employees Promotion.sql
+++ 07. Database Programmability and Transactions/01. Lab/02. Employees Promotion.sql
@@ -2,14 +2,14 @@ USE `soft_uni`;
 
 DELIMITER ~
 --
-CREATE PROCEDURE `usp_raise_salaries`(`department_name` VARCHAR(50))
+CREATE PROCEDURE `usp_raise_salaries`(p_department_name VARCHAR(50))
 BEGIN
 
     UPDATE `employees`
     SET `salary` = `salary` * 1.05
     WHERE `department_id` = (SELECT `department_id`
                              FROM `departments`
-                             WHERE `name` = `department_name`);
+                             WHERE `name` = p_department_name);
 END
 --
 ~
