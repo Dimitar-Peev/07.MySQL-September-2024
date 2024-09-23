4988e7ad404ff0eb795937f385a90a1a20e34845
Dimitar-Peev <mctrix87@abv.bg>
2024-09-23 16:57:47 +0300

    Create "02. Employees Promotion.sql"

---
+++ 07. Database Programmability and Transactions/01. Lab/02. Employees Promotion.sql
@@ -0,0 +1,16 @@
+USE `soft_uni`;
+
+DELIMITER ~
+--
+CREATE PROCEDURE `usp_raise_salaries`(`department_name` VARCHAR(50))
+BEGIN
+
+    UPDATE `employees`
+    SET `salary` = `salary` * 1.05
+    WHERE `department_id` = (SELECT `department_id`
+                             FROM `departments`
+                             WHERE `name` = `department_name`);
+END
+--
+~
+CALL `usp_raise_salaries`('Finance');
