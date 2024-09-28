1b9c736c3976ec0ebe557f7545e5274b1a344862
Dimitar-Peev <mctrix87@abv.bg>
2024-09-28 13:53:09 +0300

    Create "08. Find Full Name.sql"

--- 07. Database Programmability and Transactions/02. Exercise/07. Define Function.sql
+++ 07. Database Programmability and Transactions/02. Exercise/07. Define Function.sql
@@ -6,6 +6,8 @@ DELIMITER ~
 --
 CREATE FUNCTION ufn_is_word_comprised(set_of_letters VARCHAR(50), word VARCHAR(50))
     RETURNS INT
+    -- 0 -> word не е съставена от set_of_letters
+    -- 1 -> word е съставена от set_of_letters
     DETERMINISTIC
 BEGIN
     RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
