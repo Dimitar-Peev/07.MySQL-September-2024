f5ec052746937ea8e691c95ce270f31edf4efa87
Dimitar-Peev <mctrix87@abv.bg>
2024-09-28 13:52:43 +0300

    Create "07. Define Function.sql"

---
+++ 07. Database Programmability and Transactions/02. Exercise/07. Define Function.sql
@@ -0,0 +1,18 @@
+USE `soft_uni`;
+--
+DROP FUNCTION IF EXISTS ufn_is_word_comprised;
+
+DELIMITER ~
+--
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters VARCHAR(50), word VARCHAR(50))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+END
+--
+~
+SELECT ufn_is_word_comprised('oistmiahf', 'Sofia') AS result;
+SELECT ufn_is_word_comprised('oistmiahf', 'halves') AS result;
+SELECT ufn_is_word_comprised('bobr', 'Rob') AS result;
+SELECT ufn_is_word_comprised('pppp', 'Guy') AS result;
