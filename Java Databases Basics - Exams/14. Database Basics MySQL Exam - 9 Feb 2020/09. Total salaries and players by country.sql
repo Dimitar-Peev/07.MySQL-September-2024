a406d72a2470bbd3371cd35387430524e5858fbd
Dimitar-Peev <mctrix87@abv.bg>
2024-10-05 10:36:19 +0300

    Create "09. Total salaries and players by country.sql"

---
+++ Java Databases Basics - Exams/14. Database Basics MySQL Exam - 9 Feb 2020/09. Total salaries and players by country.sql
@@ -0,0 +1,16 @@
+USE `14_football_scout`;
+--
+SELECT c.`name`,
+       COUNT(p.`id`)   AS 'total_count_of_players',
+       SUM(p.`salary`) AS 'total_sum_of_salaries'
+FROM `countries` AS c
+         LEFT JOIN `towns` AS tw
+                   ON c.`id` = tw.`country_id`
+         LEFT JOIN `stadiums` AS s
+                   ON s.`town_id` = tw.`id`
+         LEFT JOIN `teams` AS t
+                   ON t.`stadium_id` = s.`id`
+         LEFT JOIN `players` AS p
+                   ON p.`team_id` = t.`id`
+GROUP BY c.`name`
+ORDER BY total_count_of_players DESC, c.`name`;
