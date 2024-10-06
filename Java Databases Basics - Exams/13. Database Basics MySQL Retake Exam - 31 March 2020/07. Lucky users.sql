06b592bdbbbebb65be01d202d03f38c2fea405a8
Dimitar-Peev <mctrix87@abv.bg>
2024-10-06 10:48:33 +0300

    Create "07. Lucky users.sql"

---
+++ Java Databases Basics - Exams/13. Database Basics MySQL Retake Exam - 31 March 2020/07. Lucky users.sql
@@ -0,0 +1,9 @@
+USE `13_instd`;
+--
+SELECT CONCAT_WS(' ', u.`id`, u.`username`) AS 'id_username',
+       u.`email`
+FROM `users` AS u
+         INNER JOIN `users_photos` AS up
+                    ON u.`id` = up.`user_id`
+WHERE u.`id` = up.`photo_id`
+ORDER BY u.`id`;
