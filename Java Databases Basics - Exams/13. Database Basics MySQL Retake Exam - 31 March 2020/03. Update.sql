a88099f9e520012c4386dfcad527017b92751c4a
Dimitar-Peev <mctrix87@abv.bg>
2024-10-06 10:17:49 +0300

    Create "03. Update.sql"

---
+++ Java Databases Basics - Exams/13. Database Basics MySQL Retake Exam - 31 March 2020/03. Update.sql
@@ -0,0 +1,9 @@
+USE `13_instd`;
+--
+UPDATE `addresses`
+SET `country` = (CASE LEFT(`country`, 1)
+                     WHEN 'B' THEN 'Blocked'
+                     WHEN 'T' THEN 'Test'
+                     WHEN 'P' THEN 'In Progress'
+    END)
+WHERE LEFT(`country`, 1) IN ('B', 'T', 'P');
