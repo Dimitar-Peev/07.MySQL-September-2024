271f44183c1597034325bc7738d0bc24cd942665
Dimitar-Peev <mctrix87@abv.bg>
2024-10-08 01:30:12 +0300

    Create "06. Driving schools without instructors.sql"

---
+++ 08. Exam Preparation/02. Exercise/06. Driving schools without instructors.sql
@@ -0,0 +1,11 @@
+USE `ep_go_rodie`;
+--
+SELECT dc.id, dc.name, c.brand
+FROM driving_schools dc
+         JOIN cars c
+              ON c.id = dc.car_id
+         LEFT JOIN instructors_driving_schools ids
+                   ON ids.driving_school_id = dc.id
+WHERE ids.instructor_id IS NULL
+ORDER BY c.brand, dc.id
+LIMIT 5;
