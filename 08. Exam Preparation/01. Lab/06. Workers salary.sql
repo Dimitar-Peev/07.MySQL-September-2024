d917bcfd5a1c2cff6eabdc940ecc18a1467d562f
Dimitar-Peev <mctrix87@abv.bg>
2024-10-09 19:50:43 +0300

    Create "06. Workers salary.sql"

---
+++ 08. Exam Preparation/01. Lab/06. Workers salary.sql
@@ -0,0 +1,17 @@
+USE `ep_preserves_db`;
+--
+SELECT w.id,
+       w.first_name,
+       w.last_name,
+       p.name,
+       c.country_code
+FROM workers w
+         JOIN preserves p
+              ON p.id = w.preserve_id
+         JOIN countries_preserves cp
+              ON p.id = cp.preserve_id
+         JOIN countries c
+              ON c.id = cp.country_id
+WHERE w.salary > 5000
+  AND w.age < 50
+ORDER BY c.country_code;
