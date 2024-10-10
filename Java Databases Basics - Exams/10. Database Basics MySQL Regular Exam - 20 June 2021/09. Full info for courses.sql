da6946c480234247634ce0c1aef06dd98d77fa23
Dimitar-Peev <mctrix87@abv.bg>
2024-10-10 18:25:36 +0300

    Update "09. Full info for courses.sql"

--- Java Databases Basics - Exams/10. Database Basics MySQL Regular Exam - 20 June 2021/09. Full info for courses.sql
+++ Java Databases Basics - Exams/10. Database Basics MySQL Regular Exam - 20 June 2021/09. Full info for courses.sql
@@ -1,19 +1,5 @@
 USE `10_stc`;
 --
-/*
-9.	Full information on courses
-The headquarters want us to make a query that shows the complete information about all courses in the database. The information that they need is the address, if the course is made in the Day (between 6 and 20(inclusive both)) or in the Night (between 21 and 5(inclusive both)), the bill of the course, the full name of the client, the car maker, the model and the name of the category.
-Order the results by course id.
-Required Columns
-•	name (address)
-•	day_time
-•	bill
-•	full_name (client)
-•	make
-•	model
-•	category_name (category)
-*/
-
 SELECT a.`name`,
        IF(HOUR(co.`start`) BETWEEN 6 AND 20, 'Day', 'Night') AS 'day_time',
        co.`bill`,
