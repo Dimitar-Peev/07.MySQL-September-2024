a022ab6f45e9df8e3aa3c0ecaa9b517aa68ab385
Dimitar-Peev <mctrix87@abv.bg>
2024-10-14 18:52:18 +0300

    Create "07. Accounts.sql"

---
+++ Java Databases Basics - Exams/06. Database Basics MySQL Regular Exam - 15 October 2022/07. Accounts.sql
@@ -0,0 +1,12 @@
+USE `06_restaurant_db`;
+--
+SELECT *
+FROM `waiters`
+WHERE `salary` IS NOT NULL;
+
+SELECT CONCAT(`last_name`, `first_name`, CHAR_LENGTH(`first_name`), 'Restaurant') AS 'username',
+       REVERSE(SUBSTRING(`email`, 2, 12))                                         AS 'password'
+FROM `waiters`
+WHERE `salary` IS NOT NULL
+ORDER BY `password` DESC;
+
