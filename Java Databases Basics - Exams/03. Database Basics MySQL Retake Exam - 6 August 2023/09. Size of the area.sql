0c36d82f6220ce9584a1653d2bfdb456e224f7c0
Dimitar-Peev <mctrix87@abv.bg>
2024-11-04 07:14:31 +0200

    Create "09. Size of the area.sql"

---
+++ Java Databases Basics - Exams/03. Database Basics MySQL Retake Exam - 6 August 2023/09. Size of the area.sql
@@ -0,0 +1,12 @@
+USE `03_real_estate_db`;
+--
+SELECT `address`,
+       `area`,
+       (CASE
+            WHEN `area` <= 100 THEN 'small'
+            WHEN `area` <= 200 THEN 'medium'
+            WHEN `area` <= 500 THEN 'large'
+            ELSE 'extra large'
+       END) AS 'size'
+FROM `properties`
+ORDER BY `area`, `address` DESC;
