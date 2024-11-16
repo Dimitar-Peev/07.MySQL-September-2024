ea31806b3e61f08e5625824a13f3fc8bee062049
Dimitar-Peev <mctrix87@abv.bg>
2024-11-16 17:51:11 +0200

    Create "07. Review.sql"

---
+++ Java Databases Basics - Exams/08. Database Basics MySQL Regular Exam - 13 February 2022/07. Review.sql
@@ -0,0 +1,8 @@
+USE `08_online_store`;
+--
+SELECT `id`, `content`, `rating`, `picture_url`, `published_at`
+FROM `reviews`
+WHERE `content` LIKE 'My%'
+  AND CHAR_LENGTH(`content`) > 61
+ORDER BY `rating` DESC;
+
