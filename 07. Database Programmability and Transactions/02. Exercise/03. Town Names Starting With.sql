9d8859faa2626e413f6216ad69fa2953f4100aa4
Dimitar-Peev <mctrix87@abv.bg>
2026-06-27 22:04:13 +0300

    Update "03. Town Names Starting With.sql" to add column alias for clarity

--- 07. Database Programmability and Transactions/02. Exercise/03. Town Names Starting With.sql
+++ 07. Database Programmability and Transactions/02. Exercise/03. Town Names Starting With.sql
@@ -6,7 +6,7 @@ DELIMITER ~
 --
 CREATE PROCEDURE usp_get_towns_starting_with(town_substring VARCHAR(50))
 BEGIN
-    SELECT `name`
+    SELECT `name` AS 'town_name'
     FROM `towns`
     WHERE `name` LIKE CONCAT(town_substring, '%')
     ORDER BY `name`;
