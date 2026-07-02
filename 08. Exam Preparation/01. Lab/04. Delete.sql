872f6245ef8ee072699dab16335f2d7d146ef92c
Dimitar-Peev <mctrix87@abv.bg>
2026-07-02 17:18:10 +0300

    Update "04. Delete.sql" to add DELETE statement for cleaning up related records

--- 08. Exam Preparation/01. Lab/04. Delete.sql
+++ 08. Exam Preparation/01. Lab/04. Delete.sql
@@ -1,5 +1,11 @@
 USE `ep_preserves_db`;
 --
 DELETE
+FROM countries_preserves
+WHERE preserve_id IN (SELECT id
+                      FROM preserves
+                      WHERE established_on IS NULL);
+
+DELETE
 FROM preserves
 WHERE established_on IS NULL;
