7df229f6d75e1e37527be56e425d04af175c3b04
Dimitar-Peev <mctrix87@abv.bg>
2026-07-11 20:19:31 +0300

    Update "04. Delete.sql" to add DELETE statement for cleaning up related records

--- 08. Exam Preparation/02. Exercise/04. Delete.sql
+++ 08. Exam Preparation/02. Exercise/04. Delete.sql
@@ -1,5 +1,11 @@
 USE `ep_go_rodie`;
 --
 DELETE
+FROM instructors_driving_schools
+WHERE driving_school_id IN (SELECT id
+                            FROM driving_schools
+                            WHERE night_time_driving = 0);
+
+DELETE
 FROM driving_schools
 WHERE night_time_driving = 0;
