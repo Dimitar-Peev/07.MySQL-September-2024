ee034050dbabc38185cef0f22dae9437aa323ee6
Dimitar-Peev <mctrix87@abv.bg>
2026-08-14 19:33:42 +0300

    Solution - Task 9

---
+++ Java Databases Basics - Exams/06. Database Basics MySQL Regular Exam - 15 October 2022/09. Availability.sql
@@ -0,0 +1,22 @@
+USE `06_restaurant_db`;
+--
+SELECT t.`id`                AS 'table_id',
+       `capacity`,
+       COUNT(oc.`client_id`) AS 'count_clients',
+       (
+           IF(`capacity` > COUNT(oc.`client_id`),
+              'Free seats',
+              IF(`capacity` = COUNT(oc.`client_id`),
+                 'Full',
+                 'Extra seats'
+              )
+           )
+       )                     AS 'availability'
+FROM `tables` AS t
+         INNER JOIN `orders` AS o
+                    ON o.`table_id` = t.`id`
+         INNER JOIN `orders_clients` AS oc
+                    ON o.`id` = oc.`order_id`
+WHERE `floor` = 1
+GROUP BY t.`id`
+ORDER BY t.`id` DESC;
