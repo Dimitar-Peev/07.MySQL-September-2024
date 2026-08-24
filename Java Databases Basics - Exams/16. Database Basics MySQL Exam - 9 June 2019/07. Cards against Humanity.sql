46d39e14d5e89bf43295e96a673c927d0a76b26b
Dimitar-Peev <mctrix87@abv.bg>
2026-08-24 17:44:42 +0300

    Solution - Task 7

---
+++ Java Databases Basics - Exams/16. Database Basics MySQL Exam - 9 June 2019/07. Cards against Humanity.sql
@@ -0,0 +1,10 @@
+USE `16_ruk_database`;
+--
+SELECT c.`id`,
+       CONCAT(c.`card_number`, ' : ', cl.`full_name`) AS 'card_token'
+FROM `cards` AS c
+         INNER JOIN `bank_accounts` AS ba
+                    ON c.`bank_account_id` = ba.`id`
+         INNER JOIN `clients` AS cl
+                    ON ba.`client_id` = cl.`id`
+ORDER BY c.`id` DESC;
