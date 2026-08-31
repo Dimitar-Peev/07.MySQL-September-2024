2d9bea20eadaa5fad4c0076dd942318997db48c6
Dimitar-Peev <mctrix87@abv.bg>
2026-08-31 05:04:31 +0300

    Solution - Task 12

---
+++ Java Databases Basics - Exams/17. Database Basics MySQL Exam - 21 October 2018/12. Extract all planets and their journey count.sql
@@ -0,0 +1,11 @@
+USE `17_cjms_db`;
+--
+SELECT p.`name`      AS 'planet_name',
+       COUNT(j.`id`) AS 'journeys_count'
+FROM `planets` AS p
+         INNER JOIN `spaceports` AS s
+                    ON p.`id` = s.`planet_id`
+         INNER JOIN `journeys` AS j
+                    ON s.`id` = j.`destination_spaceport_id`
+GROUP BY `planet_name`
+ORDER BY `journeys_count` DESC, `planet_name`;
