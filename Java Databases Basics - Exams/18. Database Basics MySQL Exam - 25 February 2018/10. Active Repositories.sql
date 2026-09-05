7e6f00b2c8f4aaa733c0483c8d5d6f3678fbe5d4
Dimitar-Peev <mctrix87@abv.bg>
2026-09-05 16:12:04 +0300

    Solution - Task 10

---
+++ Java Databases Basics - Exams/18. Database Basics MySQL Exam - 25 February 2018/10. Active Repositories.sql
@@ -0,0 +1,11 @@
+USE `18_buhtig`;
+--
+SELECT r.`id`,
+       r.`name`,
+       COUNT(i.`id`) AS 'issues'
+FROM `repositories` AS r
+         INNER JOIN `issues` AS i
+                    ON r.`id` = i.`repository_id`
+GROUP BY r.`id`
+ORDER BY `issues` DESC, r.`id`
+LIMIT 5;
